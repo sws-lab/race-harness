580054b8531db2ad88fcc8f76be729b03f0067d4
Jevgenijs Protopopovs <jevgenijs.protopopovs@ut.ee>
2025-05-14 12:47:10 +0300

    Drop previous concurrency detection implementation

--- harness/core/__init__.py
+++ harness/core/__init__.py
@@ -2,5 +2,4 @@ from harness.core.error import *
 from harness.core.state_graph import *
 from harness.core.process import *
 from harness.core.process_set import *
-from harness.core.concurrency import *
 from harness.core.mutual_exclusion import *
