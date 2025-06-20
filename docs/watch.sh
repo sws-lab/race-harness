a9df6cca6e0152e484e32736256ba1ff03ad8dfb
Jevgenijs Protopopovs <jevgenijs.protopopovs@ut.ee>
2025-06-20 13:58:00 +0300

    Remove the old description document

--- docs/watch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-WATCH_PATH="$1"
-COMMAND_TO_EXECUTE="${@:2}"
-
-inotifywait -m -e modify "$WATCH_PATH" --format '%w%f' |
-while read -r FILE_PATH; do
-    eval "$COMMAND_TO_EXECUTE"
-done
