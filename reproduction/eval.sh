d488b2d5a6d4de3e3f02de548d2ba69682cf6c63
Jevgenijs Protopopovs <jevgenijs.protopopovs@ut.ee>
2025-12-01 14:17:13 +0200

    Add reproduction steps

---
+++ reproduction/eval.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+OUTDIR="$(realpath $1)"
+mkdir -p "$OUTDIR"
+
+tar xvf ./linux-6.14.9.tar.xz
+pushd linux-6.14.9
+patch -p0 < ../1/patches/linux-6.14.9-minimal.patch
+popd
+
+pushd linux-kernel-goblint 
+find rh_examples -name "*.json" -print | while read TASK_FILEPATH; do
+        TASK_FILENAME="$(basename $TASK_FILEPATH)"
+        ./driver.py --db ../linux-6.14.9.db \
+                --goblint $PWD/../linux-verification-goblint/goblint \
+                --harness-compiler ./rh_generator.sh \
+                "rh_examples/$TASK_FILENAME" 2>&1 | tee "$OUTDIR/minpatch-$TASK_FILENAME.log"
+done
+popd
+
+pushd linux-6.14.9
+patch -p0 < ../1/patches/linux-6.14.9-seeded.patch
+popd
+
+pushd linux-kernel-goblint 
+find rh_examples -name "*.json" -print | while read TASK_FILEPATH; do
+        TASK_FILENAME="$(basename $TASK_FILEPATH)"
+        ./driver.py --db ../linux-6.14.9.db \
+                --goblint $PWD/../linux-verification-goblint/goblint \
+                --harness-compiler ./rh_generator.sh \
+                "rh_examples/$TASK_FILENAME" 2>&1 | tee "$OUTDIR/seeded-$TASK_FILENAME.log"
+done
+popd
