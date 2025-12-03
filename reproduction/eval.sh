df99f8afcddc20a8e619ef01747e00a75f92a1c2
Jevgenijs Protopopovs <jevgenijs.protopopovs@ut.ee>
2025-12-03 15:03:08 +0200

    Update eval script with correct paths

--- reproduction/eval.sh
+++ reproduction/eval.sh
@@ -5,28 +5,28 @@ mkdir -p "$OUTDIR"
 
 tar xvf ./linux-6.14.9.tar.xz
 pushd linux-6.14.9
-patch -p0 < ../1/patches/linux-6.14.9-minimal.patch
+patch -p0 < ../race-harness/patches/linux-6.14.9-minimal.patch
 popd
 
-pushd linux-kernel-goblint 
+pushd race-harness 
 find rh_examples -name "*.json" -print | while read TASK_FILEPATH; do
         TASK_FILENAME="$(basename $TASK_FILEPATH)"
         ./driver.py --db ../linux-6.14.9.db \
-                --goblint $PWD/../linux-verification-goblint/goblint \
+                --goblint $PWD/../race-harness-goblint/goblint \
                 --harness-compiler ./rh_generator.sh \
                 "rh_examples/$TASK_FILENAME" 2>&1 | tee "$OUTDIR/minpatch-$TASK_FILENAME.log"
 done
 popd
 
 pushd linux-6.14.9
-patch -p0 < ../1/patches/linux-6.14.9-seeded.patch
+patch -p0 < ../race-harness/patches/linux-6.14.9-seeded.patch
 popd
 
 pushd linux-kernel-goblint 
 find rh_examples -name "*.json" -print | while read TASK_FILEPATH; do
         TASK_FILENAME="$(basename $TASK_FILEPATH)"
         ./driver.py --db ../linux-6.14.9.db \
-                --goblint $PWD/../linux-verification-goblint/goblint \
+                --goblint $PWD/../race-harness-goblint/goblint \
                 --harness-compiler ./rh_generator.sh \
                 "rh_examples/$TASK_FILENAME" 2>&1 | tee "$OUTDIR/seeded-$TASK_FILENAME.log"
 done
