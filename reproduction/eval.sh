cec969941389e1ddacf1560de2be34324928d675
Jevgenijs Protopopovs <jevgenijs.protopopovs@ut.ee>
2025-12-11 11:18:30 +0200

    Fix evaluation script

--- reproduction/eval.sh
+++ reproduction/eval.sh
@@ -22,7 +22,7 @@ pushd linux-6.14.9
 patch -p0 < ../race-harness/patches/linux-6.14.9-seeded.patch
 popd
 
-pushd linux-kernel-goblint 
+pushd race-harness 
 find rh_examples -name "*.json" -print | while read TASK_FILEPATH; do
         TASK_FILENAME="$(basename $TASK_FILEPATH)"
         ./driver.py --db ../linux-6.14.9.db \
